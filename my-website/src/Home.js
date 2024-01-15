09ddbf2887a0031a5f8639c56dc3d9e3d97d070a
kieuhanh123 <ducduy0308@gmail.com>
2024-01-15 14:15:37 -0800

    added buttons to home page

--- my-website/src/Home.js
+++ my-website/src/Home.js
@@ -6,7 +6,17 @@ const Home = () => {
       <p className='name-title'> Paul Pham</p>     
       <p className='title-description'>Full-stack developer, tech-ethusiast, and a great passion for coding</p>
       
-    </div>
+      <div className="home-button">
+        <button className='button-a'>
+          <a 
+          href='https://drive.google.com/file/d/192GgJMA8BKgrMK1228AXJoTHfdlqFq5B/view?usp=sharing' 
+          target="_blank" 
+          rel="noopener noreferrer">
+          View CV</a></button>
+        <button className='button-b'><a href='#Contact'>Contact me</a></button>
+      </div>
+    </div>   
+    
   )
 }
 

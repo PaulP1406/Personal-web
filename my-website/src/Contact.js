823526a0250d559c58f69e03258d66f5b070f4fe
Duc Duy Pham <ddp4@sfu.ca>
2024-07-09 22:38:39 -0700

    Added clear form functionality after sending email

--- my-website/src/Contact.js
+++ my-website/src/Contact.js
@@ -1,9 +1,14 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import title1 from "./contact_me_text.png";
 
 const Contact = () => {
   const form = useRef();
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -19,6 +24,13 @@ const Contact = () => {
         (result) => {
           console.log(result.text);
           console.log("message sent");
+          setFormData({
+            user_name: '',
+            user_email: '',
+            message: ''
+          });
+          
+          form.current.reset();
         },
         (error) => {
           console.log(error.text);
@@ -26,23 +38,54 @@ const Contact = () => {
       );
   };
 
-  return (    
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  return (
     <div className="contact" id="contact">
-      <img src={title1} alt="title image"></img>
+      <img src={title1} alt="title image" />
       <p>Feel free to shoot a message!</p>
       <form ref={form} onSubmit={sendEmail} className="contact-form">
         <label htmlFor="user_name">Name</label>
-        <input type="text" name="user_name" id="user_name" className="input-field" required />
+        <input
+          type="text"
+          name="user_name"
+          id="user_name"
+          className="input-field"
+          value={formData.user_name}
+          onChange={handleChange}
+          required
+        />
 
         <label htmlFor="user_email">Email</label>
-        <input type="email" name="user_email" id="user_email" className="input-field" required />
+        <input
+          type="email"
+          name="user_email"
+          id="user_email"
+          className="input-field"
+          value={formData.user_email}
+          onChange={handleChange}
+          required
+        />
 
         <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" className="input-field" required></textarea>
+        <textarea
+          name="message"
+          id="message"
+          className="input-field"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        ></textarea>
 
         <input type="submit" value="Send" className="submit-button" />
       </form>
-    </div>    
+    </div>
   );
 };
 
